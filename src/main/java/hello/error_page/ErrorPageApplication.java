5708d8b424da56abda0f3e323dcfd22d193b4532
oniic <onisic95@gmail.com>
2024-07-31 15:28:03 +0900

    시작

---
+++ src/main/java/hello/error_page/ErrorPageApplication.java
@@ -0,0 +1,13 @@
+package hello.error_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/error_page/ErrorPageApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.error_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
